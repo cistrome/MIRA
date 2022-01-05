a11722de610d80c39b20b845d8c7fb4d168eac93
Allen Lynch <alynch@ds.dfci.harvard.edu>
2022-01-05 12:02:15 -0500

    added mira/info.py for metadata tracking

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
+from mira import info
 
 long_text='''
 **MIRA (Probabilistic Multimodal Models for Integrated Regulatory Analysis)** is a comprehensive methodology that systematically contrasts single cell transcription and accessibility to infer the regulatory circuitry driving cells along developmental trajectories. 
@@ -10,32 +11,29 @@ MIRA leverages joint topic modeling of cell states and regulatory potential mode
 - determine key regulators of fate decisions at branch points
 - expose the variable influence of local accessibility on transcription at distinct loci
 
-See [our manuscript](https://www.biorxiv.org/content/10.1101/2021.12.06.471401v1) for details. View code at the [MIRA github repository](https://github.com/cistrome/MIRA).
+See [our manuscript]({bioarxiv}) for details. View code at the [MIRA github repository]({github}).
 
 ## Getting Started
 
 MIRA takes count matrices of transcripts and accessible regions measured by single cell multimodal RNA-seq and ATAC-seq from any platform as input data. MIRA output integrates with AnnData data structure for interoperability with Scanpy. The initial model training is faster with GPU hardware but can be accomplished with CPU computation.
 
-Please refer to [our tutorial](https://colab.research.google.com/drive/1m3PUc_X4IaddPyHfGhwakNXFPtKEEbgp?usp=sharing) for an overview of analyses that can be achieved with MIRA using an example 10x Multiome embryonic brain dataset.
+Please refer to [our tutorial]({tutorial}) for an overview of analyses that can be achieved with MIRA using an example 10x Multiome embryonic brain dataset.
 
 
 ## System Requirements
 
-* Linux or MacOS
-* Python >=3.5, <3.8
-* (optional) CUDA-enable GPU
-
+{requirements}
 
 ## Install
 
-MIRA can be installed from either [PyPI](https://pypi.org/project/mira-multiome) or [conda-forge](https://anaconda.org/liulab-dfci/mira-multiome):
+MIRA can be installed from either [PyPI]({pypi_site}) or [conda-forge]({conda_site}):
 
 <pre>
-pip install mira-multiome
+{pypi_install}
 </pre>
 or
 <pre>
-conda install -c conda-forge -c liulab-dfci -c bioconda mira-multiome
+{conda_install}
 </pre>
 
 Installation will usually take about a minute.
@@ -53,30 +51,18 @@ To use the environment in a jupyter notebook, start the notebook server, then go
 
 ## Dependencies
 
-* pytorch
-* pyro-ppl
-* tqdm
-* moods
-* pyfaidx
-* matplotlib
-* lisa2
-* requests
-* networkx
-* numpy
-* scipy
-* optuna
-* anndata
+{dependencies}
 
-See [environment.yaml](https://github.com/cistrome/MIRA/blob/main/environment.yaml) for versioning details.
-'''
+See [environment.yaml]({github}/blob/main/environment.yaml) for versioning details.
+'''.format(**info.__dict__)
 
 
 setup(name='mira-multiome',
-      version='0.1.1',
+      version=info.version,
       description='Single-cell multiomics data analysis package',
       long_description=long_text,
       long_description_content_type="text/markdown",
-      url='https://github.com/cistrome/MIRA',
+      url = info.github,
       author='Allen W Lynch',
       author_email='alynch@ds.dfci.harvard.edu',
       license='MIT',
