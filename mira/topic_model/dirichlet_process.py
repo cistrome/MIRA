84c7eedb5d3a3ef35a99732507717a6767330be1
Allen Lynch <alynch@ds.dfci.harvard.edu>
2022-07-11 19:11:51 -0400

    added persistence to DP model

--- mira/topic_model/dirichlet_process.py
+++ mira/topic_model/dirichlet_process.py
@@ -15,7 +15,7 @@ import mira.adata_interface.core as adi
 import mira.adata_interface.topic_model as tmi
 import numpy as np
 from functools import partial
-from mira.topic_model.base import encoder_layer
+from mira.topic_model.base import encoder_layer, logger
 
 
 def mix_weights(beta):
@@ -37,10 +37,45 @@ class DP_EncoderMixin:
 
 class DPModel:
 
+    def _get_save_data(self):
+        save_data = super()._get_save_data()
+        save_data['weights']['stick_len'] = self.stick_len
+
+        return save_data
+
+    def _set_weights(self, fit_params, weights):
+        
+        stick_len = weights.pop('stick_len')
+        super()._set_weights(fit_params, weights)
+
+        self._stick_len = stick_len
+
+
+    @staticmethod
+    def _get_topic_range(stick_len, num_topics,
+        max_contribution = 0.1, min_contribution = 0.01):
+
+        expected_comp = np.power(stick_len, np.arange(num_topics))
+        
+        min_topics, max_topics = np.argmin(expected_comp > max_contribution), np.argmin(expected_comp > min_contribution)
+        return min_topics, max_topics
+
+
+    def get_topic_range(self, max_contribution = 0.1, min_contribution = 0.01):
+
+        return self._get_topic_range(self.stick_len, self.num_topics,
+            min_contribution = min_contribution, max_contribution = max_contribution)
+
+
     @property
     def stick_len(self):
-        alpha = pyro.get_param_store()['alpha_a']/pyro.get_param_store()['alpha_b']
-        return (1 - 1/(1+alpha)).cpu().detach().numpy()
+        try:
+            return self._stick_len
+        except AttributeError:
+            alpha = pyro.get_param_store()['alpha_a']/pyro.get_param_store()['alpha_b']
+            self._stick_len = (1 - 1/(1+alpha)).cpu().detach().numpy()
+
+        return self._stick_len
     
 
     def _recommend_num_topics(self, n_samples):
@@ -53,16 +88,26 @@ class DPModel:
     @staticmethod
     def boxcox(x, a):
         return ( x**a - 1)/a
-    
+
+    @staticmethod
+    def get_active_topics(topic_compositions, min_contribution = 0.05):
+        dead_topics = topic_compositions.max(0) < min_contribution
+
+        return ~dead_topics
     
     @adi.wraps_modelfunc(tmi.fetch_topic_comps, partial(adi.add_obsm, add_key = 'X_umap_features'),
         fill_kwargs=['topic_compositions', 'covariates','extra_features'])
-    def get_umap_features(self, num_topics = 20, box_cox = 0.5,*, 
+    def get_umap_features(self, box_cox = 0.5, min_contribution = 0.05,*, 
             topic_compositions, covariates, extra_features):
-        
+
+        active_topics = self.get_active_topics(topic_compositions, min_contribution)
+        num_topics = active_topics.sum()
+
+        logger.info('Found {} topics from the data.'.format(num_topics))
+
         basis = gram_schmidt_basis(num_topics)
 
-        topic_compositions = topic_compositions[:, :num_topics]
+        topic_compositions = topic_compositions[:, active_topics]
         transformed = topic_compositions/np.power(self.stick_len, np.arange(num_topics))[np.newaxis, :]
 
         return self.boxcox(transformed, box_cox).dot(basis)
