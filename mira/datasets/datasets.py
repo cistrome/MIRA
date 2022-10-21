c109a796be29b562cf44710d56d0bbea990befcc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 22:27:53 +0000

    Adding tarfile member sanitization to extractall()

--- mira/datasets/datasets.py
+++ mira/datasets/datasets.py
@@ -85,7 +85,26 @@ class Dataset:
                 assert tarfile.is_tarfile(self.local_filename)
 
                 with tarfile.open(self.local_filename) as tar:
-                    tar.extractall(self.download_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, self.download_dir)
 
                 os.remove(self.local_filename)
 
